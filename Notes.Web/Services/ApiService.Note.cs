7465883f2aba4bd3421277ace07c0e66b1a8fba8
zain <muhammedzain848@gmail.com>
2022-09-10 07:23:57 +0530

    Implemented CreateNoteCommand

---
+++ Notes.Web/Services/ApiService.Note.cs
@@ -0,0 +1,15 @@
+﻿using Notes.Web.Dtos.Notes.SaveNoteCommand;
+using Notes.Web.Services.Interfaces;
+
+namespace Notes.Web.Services;
+
+public partial class ApiService
+{
+    private const string Url = "notes";
+
+    public async Task<int> SaveNoteAsync(SaveNoteCommand request)
+    {
+        var response = await PostAsync<SaveNoteCommand, int>(request, Url);
+        return response;
+    }
+}
