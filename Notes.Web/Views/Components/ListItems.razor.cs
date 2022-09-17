1899d068bf428511207e7eef18a0d7a332fe7adf
zain <muhammedzain848@gmail.com>
2022-09-17 13:55:57 +0530

    added list items template

---
+++ Notes.Web/Views/Components/ListItems.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Notes.Web.Views.Components
+{
+    public partial class ListItems<TItem>
+    {
+        [Parameter]
+        public RenderFragment<TItem>? ItemTemplate { get; set; }
+
+        [Parameter]
+        public IEnumerable<TItem>? Items { get; set; }
+    }
+}
