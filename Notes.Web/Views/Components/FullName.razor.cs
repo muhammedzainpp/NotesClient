54b17241c48cf8238b022064ef04b536e71e332e
zain <muhammedzain848@gmail.com>
2022-09-17 22:21:38 +0530

    Changed Header Name

---
+++ Notes.Web/Views/Components/FullName.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Notes.Web.Models;
+
+namespace Notes.Web.Views.Components;
+
+public partial class FullName
+{
+    [Inject]
+    public Settings Settings { get; set; } = default!;
+}
+    
