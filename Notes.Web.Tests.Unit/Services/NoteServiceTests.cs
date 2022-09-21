8a8fce6a7e5615a6274537c7b3a965100180be89
zain <muhammedzain848@gmail.com>
2022-09-21 20:38:04 +0530

    removed unwanted

--- Notes.Web.Tests.Unit/Services/NoteServiceTests.cs
+++ Notes.Web.Tests.Unit/Services/NoteServiceTests.cs
@@ -2,7 +2,6 @@
 using Notes.Web.Models.NoteModels.SaveNoteCommand;
 using Notes.Web.Services;
 using Notes.Web.Services.Interfaces;
-using Notes.Web.ViewModels.ButtonWithSpinnerViewModels;
 using Tynamix.ObjectFiller;
 
 namespace Notes.Web.Tests.Unit.Services;
@@ -10,13 +9,11 @@ namespace Notes.Web.Tests.Unit.Services;
 public partial class NoteServiceTests
 {
     private readonly Mock<IApiService> _apiServiceMock;
-    private readonly Mock<IButtonWithSpinnerVm> _spinnerVmMock;
     private readonly INoteService _service;
 
     public NoteServiceTests()
     {
         _apiServiceMock = new Mock<IApiService>();
-        _spinnerVmMock = new Mock<IButtonWithSpinnerVm>();
         _service = new NoteService(_apiServiceMock.Object);
     }
 
