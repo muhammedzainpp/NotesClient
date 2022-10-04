be476396f8c00c32e248d45a9c260c94e0f9d0e2
zain <muhammedzain848@gmail.com>
2022-10-04 08:58:21 +0530

    added masonry layout for list notes

--- Notes.Web/Views/NoteViews/ListNoteView.razor.cs
+++ Notes.Web/Views/NoteViews/ListNoteView.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Notes.Web.ViewModels.NoteViewModels.Interfaces;
 
 namespace Notes.Web.Views.NoteViews;
@@ -8,6 +9,17 @@ public partial class ListNoteView : ComponentBase
     [Inject]
     public IListNoteVm Vm { get; set; } = default!;
 
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; } = default!;
     protected async override Task OnInitializedAsync() => 
         await Vm.GetNotesAsync();
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            var module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts/masonry.js");
+            await module.InvokeVoidAsync("initOptionsForLayout");
+        }
+    }
 }
---
+++ Notes.Web/wwwroot/scripts/masonry.js
@@ -0,0 +1,10 @@
+﻿export function initOptionsForLayout() {
+
+    setTimeout(function () {
+        var msnry = new Masonry('.row', {
+            itemSelector: '.col',
+            percentPosition: true
+        });
+        msnry.layout();
+    }, 3000);
+}
